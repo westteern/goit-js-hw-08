63ba3ffd01cc61ea36c0abf8b12dd13aae09f60e
Pavel Mikhaylenko <104693653+westteern@users.noreply.github.com>
2022-09-28 22:55:47 +0300

    Task_1_done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = galleryItems;
+
+const listRef = document.querySelector('.gallery');
+
+function markupGallery(gallery) {
+  return gallery
+    .map(
+      image => `<a class="gallery__item" href="${image.original}">
+  <img class="gallery__image" src="${image.preview}" alt="${image.description}" />
+</a>`
+    )
+    .join(' ');
+}
+const markup = markupGallery(gallery);
+listRef.innerHTML = markup;
+
+let lightboxOnClick = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
