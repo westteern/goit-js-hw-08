8c8381a65ca03f8c67cc3c3eee0ea24fca20e03e
Pavel Mikhaylenko <104693653+westteern@users.noreply.github.com>
2022-10-01 01:58:18 +0300

    in_work

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,22 @@
+import throttle from 'lodash.throttle';
 
+const FORM = 'feedback-form-state';
+
+const formData = {};
+
+const formRef = document.querySelector('.feedback-form');
+const inputRef = document.querySelector('input');
+const textaereaRef = document.querySelector('textarea');
+
+console.log(inputRef.value);
+
+if (localStorage.getItem(FORM)) {
+  inputRef.value;
+}
+// formRef.addEventListener('submit', onSubmit);
+formRef.addEventListener('input', throttle(onType, 500));
+
+function onType(e) {
+  formData[e.target.name] = e.target.value;
+  localStorage.setItem(FORM, JSON.stringify(formData));
+}
